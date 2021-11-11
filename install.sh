ae53313aa16346f564b2c73c76036bae1d2ffe9e
gregVader <kgerg13@gmail.com>
2021-11-11 21:57:25 +0100

    adwaita cube

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Adding $1 to wallpapers ..."
 
 cd $1
 sudo mkdir /usr/share/backgrounds/gnome/$1-timed
-sudo cp $1*.png /usr/share/backgrounds/gnome/$1-timed
+sudo find . -iregex '.*\.\(jpg\|png\)$' -exec cp --parents -t /usr/share/backgrounds/gnome/$1-timed '{}' '+'
 sudo cp $1-timed.xml /usr/share/backgrounds/gnome
 sudo cp $1.xml /usr/share/gnome-background-properties
 echo "Added $1 dynamic wallpaper!"
